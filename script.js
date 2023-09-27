97fda44fd0a6ccd11d551a03b97964e44b9d10c9
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-27 15:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const name = document.querySelector('#name').value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const confirmPassword = document.getElementById("confirm-password").value;
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click',(e)=>{
+	e.preventDefault();
+	console.log(e);
+	console.log(name);
+})
