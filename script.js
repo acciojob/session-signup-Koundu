e13fd2d16cbef62632d09349df68dec93e0ac953
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-27 16:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const confirmPassword = document.getElementById("confirm-password").value;
 		sessionStorage.setItem("name",name);
 		sessionStorage.setItem("email",email);
 		sessionStorage.setItem("password",password);
-		alert("Sign up successful!.")
+		alert("Sign up successful!.")	
 	}
 	else{
 		alert("Passwords do not match");
